981d212f1b82dddc82607e1f2d795fd4a83003e6
FROL <nkt.frlv7@yandex.ru>
2025-01-18 12:19:59 +0100

    configure fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LoginPage from './components/loginPage/LoginPage'
 import GamePage from './components/gamePage/GamePage'
 
