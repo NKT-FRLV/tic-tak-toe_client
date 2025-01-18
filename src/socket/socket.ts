4c6c4290e1f78aceb0a2dea54c572c0732943b7d
FROL <nkt.frlv7@yandex.ru>
2025-01-18 13:01:55 +0100

    serer url hardcoded

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from 'socket.io-client';
-const SERVER_URL = import.meta.env.VITE_SERVER_URL
+// const SERVER_URL = import.meta.env.VITE_SERVER_URL
 
-const socket: Socket = io(SERVER_URL, {
+const socket: Socket = io('https://tic-tac-toe-server-6nr9.onrender.com', {
   transports: ['websocket'],
 });
 
