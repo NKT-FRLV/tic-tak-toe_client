040b8499563b8f35169f8fafc08c2e36bead47a7
FROL <nkt.frlv7@yandex.ru>
2025-01-18 12:53:29 +0100

    server env fixed

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,6 +1,7 @@
 import { io, Socket } from 'socket.io-client';
+const SERVER_URL = import.meta.env.VITE_SERVER_URL
 
-const socket: Socket = io('http://localhost:8080', {
+const socket: Socket = io(SERVER_URL, {
   transports: ['websocket'],
 });
 
